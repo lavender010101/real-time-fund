c352c15b3039327861ed93d30e0357d95f449a90
lavender010101 <lavender010101@163.com>
2026-02-12 15:08:50 +0800

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	output: "export", // 添加这行
+	images: {
+		unoptimized: true, // 静态导出需要禁用图片优化
+	},
 };
 
 module.exports = nextConfig;
